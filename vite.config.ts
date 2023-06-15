65acc2b8b510d34630a7176c7ebd829bb2e172b6
barrett-ethdotco <123409972+barrett-ethdotco@users.noreply.github.com>
2023-06-15 14:42:21 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import { splitVendorChunkPlugin } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [splitVendorChunkPlugin(), react()],
+  server: {
+    port: 3000,
+  },
 });
